5b7eddf70fc22e25e9b9ed840ee4f0f1aa5634bc
Kiarash-J <kiarashjg@gmail.com>
2026-05-21 22:41:15 -0400

    added effect subclasses, will continue working on them

--- Assets/Scripts/Spells/SpellScript.cs
+++ Assets/Scripts/Spells/SpellScript.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Spell
@@ -8,10 +9,10 @@ public class Spell
 
     private float _damage;
 
-    private string _effect;
+    private Effect _effect;
 
     public bool learned;
-    public Spell(string element, string spellname, float? damage = null, string effect = null)
+    public Spell(string element, string spellname, float? damage = null, Effect effect = null)
     {
         this.name = spellname;
         this._element = element;
@@ -32,16 +33,56 @@ public class Spell
 }
 public class Effect
 {
-    private float _damage;
-    private int _turntime;
+    private int _baseDamage;
+    private int _count;
     private string _effectName;
-    private int _effectTier;
+    protected int _effectTier;
 
-    public Effect(float damage, int turntime, string effectName, int effectTier)
+    public Effect(int damage, int count, string effectName, int effectTier)
     {
-        this._damage = damage;
-        this._turntime = turntime;
+        this._baseDamage = damage;
+        this._count = count;
         this._effectName = effectName;
         this._effectTier = effectTier;
     }
+    
+    public virtual double Activate()
+    {
+        this._count--;
+        return this._baseDamage * (1 + (0.15 * this._effectTier)) ;
+    }
+}
+public class Burn : Effect //deals set damage every round end
+{
+    public Burn(int count, int  effectTier) : base(5, count, "Immolate", effectTier) { }
+
+    public override double Activate() // ac
+    {
+
+        return base.Activate();
+    }
+}
+
+public class WaterPrism : Effect //stores damage over a set amount of turns, multiplies and applies them all at once when it expires
+{
+    private double _storedDamage;
+    public WaterPrism(int count, int effectTier) : base(0, count, "WaterPrism", effectTier) { }
+
+    public override double Activate() // activates once turn ends with one count
+    {
+        return _storedDamage * (1 + (0.2 * base._effectTier));
+        
+    }
+    
+}
+
+public class Impaled : Effect //deals damage every time the enemy makes an action
+{
+    public int spikenumber;
+    public Impaled(int count, int spikenumber) : base(5, count, "Impaled", 0)
+    {
+        this.spikenumber = spikenumber;
+    }
+
+    
 }
