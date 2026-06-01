429483ad189729c340b092d154caff4aefe7afa6
Demoincc <marcelltzaras@gmail.com>
2026-06-01 12:24:09 -0400

    Added Interactable Buttons on the Inventory

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -5,11 +5,12 @@ using UnityEngine.InputSystem;
 public class InventoryUI : MonoBehaviour
 {
     [SerializeField] GameObject Inventory_Bg;
-    private bool _isOpen;
+    [SerializeField] GameObject Icon;
+    private bool _isInvOpen;
     
     public void Start()
     {
-        _isOpen = false;
+        _isInvOpen = false;
     }
     
     public void Update()
@@ -19,11 +20,11 @@ public class InventoryUI : MonoBehaviour
 
     public void ReadInput()
     {
-        if (Keyboard.current.eKey.wasPressedThisFrame && _isOpen == false) 
+        if (Keyboard.current.eKey.wasPressedThisFrame && _isInvOpen == false) 
         {
             DisplayInventory(new Vector3(-950,-301,0), new Vector3(-10,0,0));
         }
-        else if (Keyboard.current.eKey.wasPressedThisFrame && _isOpen == true) 
+        else if (Keyboard.current.eKey.wasPressedThisFrame && _isInvOpen == true) 
         {
             CloseInventory();
         }
@@ -31,7 +32,7 @@ public class InventoryUI : MonoBehaviour
 
     private void DisplayInventory(Vector3 startPosition, Vector3 spacingOffset)
     {
-        this._isOpen = true;
+        this._isInvOpen = true;
         PlayerMovement.IsInventoryOpen = true;
         Time.timeScale = 0f;
         Inventory_Bg.SetActive (true);
@@ -41,11 +42,24 @@ public class InventoryUI : MonoBehaviour
 
     private void CloseInventory()
     {
-        this._isOpen = false;
+        this._isInvOpen = false;
         PlayerMovement.IsInventoryOpen = false;
         Time.timeScale = 1f;
         Inventory_Bg.SetActive (false);
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+
+    public void DisplayPotions()
+    {
+        // Display the inventory icons as per the amount of potions
+    }
+    public void DisplayHats()
+    {
+        // Display the inventory icons as per the amount of hats
+    }
+    public void DisplayRunes()
+    {
+        // Display the inventory icons as per the amount of runes
     }
 }
